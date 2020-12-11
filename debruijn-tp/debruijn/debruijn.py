6899f85b3597117faa8e5f1eeee097dad859d7bd
Sarah Rajaosafara <sarah.rajaosafara@student.griffith.ie>
2020-12-11 15:09:52 +0100

    Modification remove_paths et simplify_bubbles + Commentaires

--- debruijn-tp/debruijn/debruijn.py
+++ debruijn-tp/debruijn/debruijn.py
@@ -24,6 +24,7 @@ random.seed(9001)
 from random import randint
 import statistics
 import matplotlib.pyplot as plt
+import itertools
 
 __author__ = "Your Name"
 __copyright__ = "Universite Paris Diderot"
@@ -68,24 +69,28 @@ def get_arguments():
 def read_fastq(fastq_file):
     with open(fastq_file, "rt") as myfile:
         for line in myfile:
+            # Remove the '\n' at the end of each line
             yield next(myfile).replace('\n', '')
             next(myfile)
             next(myfile)
 
 def cut_kmer(read, kmer_size):
     for i, letter in enumerate(read):
+        # Only take kmer of size kmer_size
         if i<=len(read)-kmer_size:
             yield read[i:i+kmer_size]
 
 
 def build_kmer_dict(fastq_file, kmer_size):
-    lines = read_fastq(fastq_file)
     kmers = []
+    # Get the list of kmers
+    lines = read_fastq(fastq_file)
     for line in lines:
         for kmer in cut_kmer(line, kmer_size):
             kmers.append(kmer)
 
     dict_kmers = dict()
+    # For each kmer, if it is not in the dict, add occurence of 1, else increment the occurence
     for kmer in kmers:
         if kmer not in dict_kmers.keys():
             dict_kmers[kmer]=1
@@ -95,33 +100,41 @@ def build_kmer_dict(fastq_file, kmer_size):
 
 def build_graph(kmer_dict):
     graph = nx.DiGraph()
+    # Divide each kmer in two parts and add a weighted edge
     for kmer, weight in kmer_dict.items():
         graph.add_edge(kmer[:(len(kmer)-1)], kmer[1:], weight=weight)
     return graph
 
 def remove_paths(graph, path_list, delete_entry_node, delete_sink_node):
+    # For each path, delete the nodes between the entry and sink nodes if they exist in the graph
     for path in path_list:
         if not delete_entry_node and not delete_sink_node:
             for i in range(1,len(path)-1):
-                graph.remove_node(path[i])
+                if path[i] in graph.nodes:
+                    graph.remove_node(path[i])
         elif delete_entry_node and not delete_sink_node:
             for i in range(0,len(path)-1):
-                graph.remove_node(path[i])
+                if path[i] in graph.nodes:
+                    graph.remove_node(path[i])
         elif not delete_entry_node and delete_sink_node:
             for i in range(1,len(path)):
-                graph.remove_node(path[i])
+                if path[i] in graph.nodes:
+                    graph.remove_node(path[i])
         else:
             for i in range(0,len(path)):
-                graph.remove_node(path[i])
+                if path[i] in graph.nodes:
+                    graph.remove_node(path[i])
     return graph
 
 def std(data):
+    # Return the standard deviation
     return statistics.stdev(data)
-
 
 def select_best_path(graph, path_list, path_length, weight_avg_list,
                      delete_entry_node=False, delete_sink_node=False):
 
+    # Get the list of the paths with an average weight equal to the max average weight
+    # Get the list of their corresponding lengths
     heavier_val = max(weight_avg_list)
     heavier_list = []
     new_lengths = []
@@ -130,48 +143,72 @@ def select_best_path(graph, path_list, path_length, weight_avg_list,
             heavier_list.append(path_list[i])
             new_lengths.append(path_length[i])
 
+    # Get the list of the paths with a length equal to the max length among the paths in the new list
     longest_val = max(new_lengths)
     longest_list = []
     for i in range(len(heavier_list)):
         if new_lengths[i] == longest_val:
             longest_list.append(heavier_list[i])
 
+    # Select a random path among the paths in the new list
     random_index = random.randint(0,len(longest_list)-1)
+
+    # Remove it from the initial path_list
     path_list.remove(longest_list[random_index])
 
+    # Remove all the paths in path_list
     return remove_paths(graph, path_list, delete_entry_node, delete_sink_node)
 
 
 def path_average_weight(graph, path):
     s=0
+    # Add all the weights
     for i in range(len(path)-1):
         s += graph[path[i]][path[i+1]]['weight']
+    # Divide by the number of weights
     return s/(len(path)-1)
 
 def solve_bubble(graph, ancestor_node, descendant_node):
+    # Get all the paths between the ancestor node and the descendant node
     all_paths = nx.all_simple_paths(graph,ancestor_node,descendant_node)
     all_paths = list(all_paths)
 
+    # Get a list with all the lengths
+    # Get a list with all the average weights
     lengths = []
     weights = []
     for path in all_paths:
         lengths.append(len(path))
         weights.append(path_average_weight(graph,path))
 
+    # Select the best path among these paths
     return select_best_path(graph, all_paths, lengths, weights)
 
 def simplify_bubbles(graph):
     couples = []
-
+    # For each node
     for e in graph.nodes:
+        # Get a list of all the predecessors
         preds = list(graph.predecessors(e))
+        ancestors = []
+        # If there is more than one predecessor
         if len(preds)>1:
-            s = nx.lowest_common_ancestor(graph, preds[0], preds[1], None)
-            if s is not None:
+            # Look at the lowest common ancestor between each pair of predecessors
+            for node1, node2 in itertools.combinations(preds,2):
+                # If there is one, add it to the list of ancestors
+                # (there is a bubble between s and e)
+                s = nx.lowest_common_ancestor(graph, node1, node2, None)
+                if s is not None:
+                    ancestors.append(s)
+            # Get a list of couples representing a bubble
+            for s in list(set(ancestors)):
                 couples.append((s,e))
 
+    # Remove each bubble
     for s,e in couples:
-        graph = solve_bubble(graph,s,e)
+        # If the node has not been removed yet
+        if s in graph.nodes and e in graph.nodes:
+            graph = solve_bubble(graph,s,e)
 
     return graph
 
@@ -183,20 +220,24 @@ def solve_out_tips(graph, ending_nodes):
 
 def get_starting_nodes(graph):
     nodes = []
+    # For each node, count the number of predecessors
     for node in graph.nodes:
         count=0
         for p in graph.predecessors(node):
             count +=1
+        # If it has no predecessor, it is a starting node
         if count==0:
             nodes.append(node)
     return nodes
 
 def get_sink_nodes(graph):
     nodes = []
+    # For each node, count the number of successors
     for node in graph.nodes:
         count=0
         for p in graph.successors(node):
             count +=1
+        # If it has no successor, it is a sink node
         if count==0:
             nodes.append(node)
     return nodes
@@ -205,12 +246,14 @@ def get_contigs(graph, starting_nodes, ending_nodes):
     contigs = []
     for s in starting_nodes:
         for e in ending_nodes:
-            #all paths between s and e
+            # Get all the paths between s and e
             all_paths = nx.all_simple_paths(graph,s,e)
             all_paths = list(all_paths)
 
             for path in all_paths:
+                # Get the content of the first node
                 contig = path[0]
+                # Add the last letter of all the other nodes to the contig
                 for st in path[1:]:
                     contig += st[len(st)-1:]
                 contigs.append((contig,len(contig)))
@@ -222,6 +265,7 @@ def fill(text, width=80):
 
 
 def save_contigs(contigs_list, output_file):
+    # Write each contig to the file
     with open(output_file, "wt") as myfile:
         for i, (contig, len_contig) in enumerate(contigs_list):
             myfile.write(">contig_"+str(i)+ " len="+str(len_contig)+"\n")
@@ -271,6 +315,20 @@ def main():
 
     #path = ['TCAGAGCTCTAGAGTTGGTT', 'CAGAGCTCTAGAGTTGGTTC']
     #print(path_average_weight(graph, path))
+
+    graph_1 = nx.DiGraph()
+    graph_1.add_weighted_edges_from([(3,2,1),(2,8,2),(8,6,3),(6,7,4),(2,4,5),(4,7,6),
+                                    (10,7,7),(3,12,8),(12,11,9),(11,7,10)])
+    graph_1 = simplify_bubbles(graph_1)
+
+    print(graph_1.edges)
+
+    graph_2 = nx.DiGraph()
+    graph_2.add_weighted_edges_from([(3,2,1),(2,8,2),(8,9,3),(9,5,4),(5,6,5),(2,4,6),
+                                    (4,9,7),(4,5,8),(2,10,9),(10,5,10),(5,7,11)])
+    graph_2 = simplify_bubbles(graph_2)
+
+    print(graph_2.edges)
 
 
 if __name__ == '__main__':
